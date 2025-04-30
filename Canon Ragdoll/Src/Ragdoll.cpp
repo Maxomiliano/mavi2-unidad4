d5c65d306b83ec99b7a364f38c0748cf530aa564
Maxomiliano <max.nebula1@gmail.com>
2025-04-30 15:42:29 -0300

    Se corrige el error de posicion del Ragdoll para que se instancie desde el cañon

--- Canon Ragdoll/Src/Ragdoll.cpp
+++ Canon Ragdoll/Src/Ragdoll.cpp
@@ -8,49 +8,56 @@ Ragdoll::Ragdoll(b2World* world, const b2Vec2& position)
 	float friction = 0.5f;
 	float restitution = 0.2f;
 
+	b2Vec2 torsoOffset(0.0f, 0.0f);
+	b2Vec2 headOffset(0.0f, -5.0f);
+	b2Vec2 leftArmOffset(-2.5f, 0.0f);
+	b2Vec2 rightArmOffset(2.5f, 0.0f);
+	b2Vec2 leftLegOffset(-1.5f, 5.0f);
+	b2Vec2 rightLegOffset(1.5f, 5.0f);
+
 	headBody = Box2DHelper::CreateRectangularDynamicBody(world, 2.5f, 2.5f, density, friction, restitution);
-	headBody->SetTransform(b2Vec2(25.0f, 30.0f), 0.0f);
+	headBody->SetTransform(position + headOffset, 0.0f);
 
 	torsoBody = Box2DHelper::CreateRectangularDynamicBody(world, 4, 9, density, friction, restitution);
-	torsoBody->SetTransform(b2Vec2(25.0f, 35.0f), 0.0f);
+	torsoBody->SetTransform(position + torsoOffset, 0.0f);
 
 	leftArmBody = Box2DHelper::CreateRectangularDynamicBody(world, 1, 6, density, friction, restitution);
-	leftArmBody->SetTransform(b2Vec2(22.5f, 35.0f), 0.0f);
+	leftArmBody->SetTransform(position + leftArmOffset, 0.0f);
 
 	rightArmBody = Box2DHelper::CreateRectangularDynamicBody(world, 1, 6, density, friction, restitution);
-	rightArmBody->SetTransform(b2Vec2(27.5f, 35.0f), 0.0f);
+	rightArmBody->SetTransform(position + rightArmOffset, 0.0f);
 
 	leftLegBody = Box2DHelper::CreateRectangularDynamicBody(world, 1, 6, density, friction, restitution);
-	leftLegBody->SetTransform(b2Vec2(24.0f, 42.8f), 0.0f);
+	leftLegBody->SetTransform(position + leftLegOffset, 0.0f);
 
 	rightLegBody = Box2DHelper::CreateRectangularDynamicBody(world, 1, 6, density, friction, restitution);
-	rightLegBody->SetTransform(b2Vec2(26.5f, 42.8f), 0.0f);
+	rightLegBody->SetTransform(position + rightLegOffset, 0.0f);
 
 
 	//Cabeza y torso
-	b2Vec2 headAnchor(25.0f, 30.5f);
-	b2Vec2 centerAnchor(25.0f, 30.0f);
+	b2Vec2 headAnchor = position + headOffset + b2Vec2(0.0f, 1.0f);
+	b2Vec2 centerAnchor = position + torsoOffset + b2Vec2(0.0f, -4.0f);
 	b2DistanceJoint* headJoint = Box2DHelper::CreateDistanceJoint(world, headBody, headAnchor, torsoBody, centerAnchor, 0, 0.5f, 0.2f);
 
 	
 	//Brazo izquierdo y torso
-	b2Vec2 centerAnchorLA(23.0f, 31.0f);
-	b2Vec2 leftArmAnchor(22.5f, 32.5f);
+	b2Vec2 centerAnchorLA = position + torsoOffset + b2Vec2(-2.0f, -4.0f);
+	b2Vec2 leftArmAnchor = position + leftArmOffset + b2Vec2(0.0f, -3.0f);
 	b2DistanceJoint* leftArmJoint = Box2DHelper::CreateDistanceJoint(world, leftArmBody, leftArmAnchor, torsoBody, centerAnchorLA, 1, 0.5f, 0.2f);
 
 	//Brazo derecho y torso
-	b2Vec2 centerAnchorRA(26.5f, 31.0f);
-	b2Vec2 rightArmAnchor(27.7f, 32.0f);
+	b2Vec2 centerAnchorRA = position + torsoOffset + b2Vec2(2.0f, -4.0f);
+	b2Vec2 rightArmAnchor = position + rightArmOffset + b2Vec2(0.0f, -3.0f);
 	b2DistanceJoint* rightArmJoint = Box2DHelper::CreateDistanceJoint(world, rightArmBody, rightArmAnchor, torsoBody, centerAnchorRA, 1, 0.8f, 0.1f);
 
 	//Pierna izquierda y torso
-	b2Vec2 centerAnchorLL(23.5f, 39.0f);
-	b2Vec2 leftLegAnchor(23.5f, 39.5f);
+	b2Vec2 centerAnchorLL = position + torsoOffset + b2Vec2(-1.0f, 4.0f);
+	b2Vec2 leftLegAnchor = position + leftLegOffset + b2Vec2(0.0f, -3.0f);
 	b2DistanceJoint* leftLegJoint = Box2DHelper::CreateDistanceJoint(world, leftLegBody, leftLegAnchor, torsoBody, centerAnchorLL, 1, 0.5f, 0.2f);
 
 	//Pierna derecha y torso
-	b2Vec2 centerAnchorRL(26.0f, 39.0f);
-	b2Vec2 rightLegAnchor(26.0f, 39.5f);
+	b2Vec2 centerAnchorRL = position + torsoOffset + b2Vec2(1.0f, 4.0f);
+	b2Vec2 rightLegAnchor = position + rightLegOffset + b2Vec2(0.0f, -3.0f);
 	b2DistanceJoint* rightLegJoint = Box2DHelper::CreateDistanceJoint(world, rightLegBody, rightLegAnchor, torsoBody, centerAnchorRL, 1, 0.5f, 0.2f);
 	/*
 	*/
